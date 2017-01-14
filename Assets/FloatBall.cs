ab7541b4c9b75cd892f8a6799131152f09c2db58
Genz1 <genzivr@gmail.com>
2017-01-13 18:15:49 -0700

    Added more design and setting

---
+++ Assets/FloatBall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.GetKeyDown("t"))
+        {
+            GetComponent<Animation>().Play("Sphere|Float");
+        }
+	}
+}
