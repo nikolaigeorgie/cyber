9aa55ca40709146880710355cb9567c44c77ed87
Genz1 <genzivr@gmail.com>
2017-03-08 16:37:45 -0700

    Added script to checkout, red purchase works. Unique ID Generated for user and purchase

---
+++ Assets/Scripts/Purchasing/Checkout.cs
@@ -0,0 +1,57 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+using SimpleFirebaseUnity;
+using SimpleFirebaseUnity.MiniJSON;
+
+using System.Collections.Generic;
+using System.Collections;
+using System;
+using System.IO;
+
+public class Checkout : MonoBehaviour {
+
+	System.Guid myGUID = System.Guid.NewGuid();
+
+	public static string rxsRedPurchased;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void checkedOUT(){
+
+		StartCoroutine (checkoutCompleted ());
+
+	}
+
+	string customerId = "124211234";
+
+	IEnumerator checkoutCompleted() {
+
+		rxsRedPurchased = RedPurchase.rxsRedShoeDetails;
+
+		Firebase firebase = Firebase.CreateNew ("fir-test-884fd.firebaseio.com");
+
+		FirebaseQueue firebaseQueue = new FirebaseQueue();
+
+		//firebaseQueue.AddQueuePush(firebase.Child("order/customer", true), secondDetails + newDetails, true);
+		firebaseQueue.AddQueuePush(firebase.Child("customers/" + myGUID, true), rxsRedPurchased, true);
+		firebaseQueue.AddQueuePush(firebase.Child("customers/" + myGUID + "/Shoe", true), rxsRedPurchased, true);
+
+		yield return new WaitForSeconds (15f);
+
+		yield return null;
+
+
+	}
+
+}
--- Assets/Scripts/Purchasing/Shoes/RedPurchase.cs
+++ Assets/Scripts/Purchasing/Shoes/RedPurchase.cs
@@ -3,6 +3,24 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+using SimpleFirebaseUnity;
+using SimpleFirebaseUnity.MiniJSON;
+
+using System.Collections.Generic;
+using System.Collections;
+using System;
+using System.IO;
+
+[System.Serializable]
+public class RxsShoeRed{
+
+	public int station;
+	public string productName;
+	public string color;
+	public int size;
+	public int quantity;
+	public int totalPrice;
+}
 
 public class RedPurchase : MonoBehaviour {
 
@@ -12,20 +30,39 @@ public class RedPurchase : MonoBehaviour {
 	public GameObject finalMessage;
 	public GameObject purchaseOption;
 
+	RxsShoeRed rxsShoe = new RxsShoeRed ();
+
+	public static string rxsRedShoeDetails;
+
+	//Debug.Log (rxsRedShoeDetails);
+
 	public void SizeButton()
 	{
 		//            Debug.Log("Red Button Clicked");
 		//			size5.SetActive(false);
 
+		rxsShoe.station = 3;
+		rxsShoe.productName = "Revenge X Storm";
+		rxsShoe.color = "Red";
+		rxsShoe.size = 9;
+
+
 		sizeSelection.SetActive (false);
 		quantitySelection.SetActive(true);
+
+
+
 	}
 
 
 	public void QuantityButton() {
 
+		rxsShoe.quantity = 1;
+		rxsShoe.totalPrice = 200;
 		quantitySelection.SetActive (false);
 		finalMessage.SetActive (true);
+		rxsRedShoeDetails = JsonUtility.ToJson (rxsShoe);
+
 	}
 
 
@@ -33,6 +70,11 @@ public class RedPurchase : MonoBehaviour {
 
 		purchaseOption.SetActive (false);
 		sizeSelection.SetActive (true);
+
 	}
+
+
+
+		
 
 }
